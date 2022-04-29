742dfd5d6b3f4885a98326cb01a66d941a9602d8
omgmajk <majkieboy@gmail.com>
2022-04-29 20:18:15 +0200

    Stats and rating
    Added new components regarding feedback stats plus styling.

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 
 	import FeedbackList from './components/FeedbackList.svelte';
+	import FeedbackStats from './components/FeedbackStats.svelte';
 
 	let feedback = [
 		{
@@ -18,11 +19,21 @@
 			rating: 8,
 			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.'
 		}
-	]
+	];
+
+	$: count = feedback.length;
+	$: average = feedback.reduce((a, {rating}) => a + rating, 0) / feedback.length;
+
+	const deleteFeedback = (e) => {
+		let itemId = e.detail;
+		feedback = feedback.filter((item) => item.id != itemId);
+	}
+
 </script>
 
-<main>
-	<FeedbackList {feedback} /> <!-- feedback={feedback} is the same as {feedback}-->
+<main class="container"> <!-- Can have class directly on the <main> tag -->
+	<FeedbackStats {count} {average} />
+	<FeedbackList {feedback} on:delete-feedback="{deleteFeedback}" /> <!-- feedback={feedback} is the same as {feedback}-->
 </main>
 
 <style>
